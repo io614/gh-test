129b666c1f17ba49670cc37a5433f643d15297b7
io614 <ivanongjiaen@gmail.com>
2023-09-11 15:43:27 +0800

    add

--- hello.py
+++ hello.py
@@ -3,6 +3,12 @@ import os
 # List all files in a directory using os.listdir
 basepath = 'my_directory/'
 hello = 'world'
+
+def add(num):
+  return num+1
+
+add(3)
+
 for entry in os.listdir(basepath):
   if os.path.isfile(os.path.join(basepath, entry)):
     print(entry)
