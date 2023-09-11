0026c0be118c321a355e4cb84ebd653105a2b401
io614 <ivanongjiaen@gmail.com>
2023-09-11 15:40:48 +0800

    hello

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import os
 
 # List all files in a directory using os.listdir
 basepath = 'my_directory/'
+hello = 'world'
 for entry in os.listdir(basepath):
   if os.path.isfile(os.path.join(basepath, entry)):
     print(entry)
